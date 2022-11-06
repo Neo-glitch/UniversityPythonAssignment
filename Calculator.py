cf01749f34b50dba5463c8e812814c5878755054
Nweke Onyekachukwu <55864433+Neo-glitch@users.noreply.github.com>
2022-11-06 19:00:27 +0100

    fixed bug in calculator program

--- Calculator.py
+++ Calculator.py
@@ -28,14 +28,15 @@ class Calculator:
         self.__get_inputs(a, b)
 
     def __show_operator_code_request(self):
-        print("Choose One of the Operator Codes below!!")
-        print("For Addition Enter '1'\nFor Subtraction Enter '2'\n"
-              "For Division Enter '3'\nFor Multiplication Enter '4'")
+        print("\nChoose One of the Operator Codes below!!\n")
+        print("\tFor Addition Enter '1'\n\tFor Subtraction Enter '2'\n"
+              "\tFor Division Enter '3'\n\tFor Multiplication Enter '4'")
         try:
-            operator = int(input("Enter here: "))
+            operator = int(input("\nEnter here: "))
             self.operator_code = operator
+            self.__calculate()
         except ValueError:
-            print("You Entered the wrong operator code, please check and try again")
+            print("!!You Entered the wrong operator code, please check and try again!!")
             self.__show_operator_code_request()
 
     def __get_inputs(self, first_number, second_number):
@@ -54,8 +55,12 @@ class Calculator:
             result = self.first_no - self.second_no
         elif self.operator_code == 3:
             result = self.first_no / self.second_no
-        else:
+        elif self.operator_code == 4:
             result = self.first_no * self.second_no
+        else:
+            print("Invalid operator code, Please Enter a result from from the one listed above")
+            self.__show_operator_code_request()
+            return
 
         print(f"The result of your calculation is: {result}")
 
@@ -63,4 +68,3 @@ class Calculator:
         print("Welcome to Neo's Calculator App\n------------------------------")
         self.__show_input_request()
         self.__show_operator_code_request()
-        self.__calculate()
